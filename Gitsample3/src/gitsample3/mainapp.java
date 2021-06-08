09afc5ea08bdb974adb8fb29367c52a21a214e02
Kevin <kevinyen0927@gmail.com>
2021-06-08 15:54:50 +0800

    1st Version

---
+++ Gitsample3/src/gitsample3/mainapp.java
@@ -0,0 +1,11 @@
+package gitsample3;
+
+public class mainapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//這是測試第一版
+		System.out.println("這是測試第一版");
+	}
+
+}
